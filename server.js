54191d493450dd9a7996befd6e1323efd3083b75
Alex <kuzminbass@gmail.com>
2022-01-17 20:35:43 +1000

    env

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(
-  `mongodb+srv://Alex:chicken1@cluster0.qlq30.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-  { useNewUrlParser: true }
-);
+mongoose.connect(`${process.env.DATABASE_URL}`, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
@@ -19,9 +18,5 @@ app.use(express.json());
 
 const subscribersRouter = require("./routes/subscribers");
 app.use("/subscribers", subscribersRouter);
-
-
-
-
 
 app.listen(3000, () => console.log("server started"));
